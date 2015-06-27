776d19e27734f8ab60deebe456537088861c8434
Taras Slipets <taras.slipets@gmail.com>
2015-06-27 13:42:30 +0100

    added MySQL db support

--- src/main/java/ua/jug/chuckquotes/user/UserRegistry.java
+++ src/main/java/ua/jug/chuckquotes/user/UserRegistry.java
@@ -2,13 +2,14 @@ package ua.jug.chuckquotes.user;
 
 import ua.jug.chuckquotes.exceptions.UserNotExistException;
 import ua.jug.chuckquotes.user.dao.HardCodedUserDao;
+import ua.jug.chuckquotes.user.dao.MySQLUserDao;
 import ua.jug.chuckquotes.user.dao.UserDao;
 
 public class UserRegistry {
     private UserDao dao;
 
     public UserRegistry() {
-        this.dao = new HardCodedUserDao();
+        this.dao = new MySQLUserDao();
     }
 
     public boolean authenticate(String username, String password) throws UserNotExistException {
--- src/main/java/ua/jug/chuckquotes/user/dao/MySQLUserDao.java
+++ src/main/java/ua/jug/chuckquotes/user/dao/MySQLUserDao.java
@@ -1,7 +1,36 @@
 package ua.jug.chuckquotes.user.dao;
 
-/**
- * Created by Taras on 15.05.2015.
- */
-public class MySQLUserDao {
+import ua.jug.chuckquotes.exceptions.UserNotExistException;
+import ua.jug.chuckquotes.user.User;
+
+import java.sql.*;
+
+public class MySQLUserDao implements UserDao{
+    @Override
+    public boolean authenticate(String login, String password) throws UserNotExistException {
+        final User user = getUser(login);
+        return user.getName().equals(login) && user.getPassword().equals(password);
+    }
+
+    @Override
+    public User getUser(String login) throws UserNotExistException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            final Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chuck_quotes", "root", "root");
+
+            final PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE username LIKE ?");
+            preparedStatement.setString(1, login);
+            final ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                final String username = resultSet.getString("username");
+                final String password = resultSet.getString("password");
+
+                return new User(username, password);
+            }
+
+        } catch (Exception e) {
+            throw new UserNotExistException(login);
+        }
+        throw new UserNotExistException(login);
+    }
 }
